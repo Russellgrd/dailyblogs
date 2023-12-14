6d38117fd2f1cb152f2c7dd9315b76b62fade97d
unknown <russell_driver@ymail.com>
2023-12-14 07:29:52 +0000

    layout styles added to Posts component

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext, ReactHTMLElement } from 'react'
+import { useEffect, useState, useContext, ReactHTMLElement, Fragment } from 'react'
 import moment from 'moment';
 import { UserContext } from '../context/UserContext';
 import { useIsAuth } from '../helpers/useIsAuth';
@@ -6,6 +6,8 @@ import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Bars } from 'react-loading-icons'
 import Editpost from './Editpost';
+import navBurger from '../assets/nav-burgerIcon.svg';
+
 
 const Posts = () => {
     const userContext = useContext(UserContext);
@@ -13,11 +15,15 @@ const Posts = () => {
     let [blogDeleted, setBlogDeleted] = useState<any>(null);
     let [deletingOrUpdatingBlog, setDeletingOrUpdatingBlog] = useState<any>(false);
     let [editing, setEditing] = useState<any>(false);
+    let [postsButtons, setPostsButtons] = useState(false);
 
     const navigate = useNavigate();
 
     let userNotification = "postUserNotificationBox";
     blogDeleted ? userNotification = "postUserNotificationBox showMessage" : "postUserNotificationBox";
+
+    let postsButtonWrapper: string = ""
+    postsButtons == true ? postsButtonWrapper = "showPostsButtons" : postsButtonWrapper = "postsButtonsWrapper"
 
     useEffect(() => {
         const getPosts = async () => {
@@ -81,12 +87,30 @@ const Posts = () => {
         })
     }
 
+    const handlePostOptions = (e: React.MouseEvent) => {
+        console.log(e.target);
+        setPostsButtons(state => !state);
+    }
+
     return (
         <div className='posts'>
-
             {blogs && blogs.map((blog: { _id: string, blogTitle: string, blogBody: string, blogImageName: string, createdAt: string, email: string }) =>
                 <div className='post' key={blog._id}>
-                    {userContext.user.email === blog.email ? <div className='postsButtonBox'> <button className='postsEditDeleteButton' onClick={(e) => { handleDelete(blog) }}>delete</button> <button className='postsEditDeleteButton' onClick={(e) => { handleEdit(blog) }}>edit</button>  </div> : null}
+                    {userContext.user.email === blog.email ?
+                        <div className='postsMenu'>
+                            <img onClick={(e) => { handlePostOptions(e) }} className="postsBurger" src={navBurger} alt="Nav Burger Icon" />
+                            <div className="postsButtonBox">
+                                <div className={postsButtonWrapper}>
+                                    <button className='postsButton' onClick={(e) => { handleDelete(blog) }}>
+                                        delete
+                                    </button>
+                                    <button className='postsButton' onClick={(e) => { handleEdit(blog) }}>
+                                        edit
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        : null}
                     <h1 className='postHeading'>{blog.blogTitle}</h1>
                     <p>Written {moment(blog.createdAt).format('DD-MM-YYYY HH:mm')} by {blog.email.split("@")[0]}</p>
                     {deletingOrUpdatingBlog ? <Bars /> : null}
